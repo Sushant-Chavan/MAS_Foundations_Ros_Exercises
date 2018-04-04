1f8d9b4935aa6117a848dde54580baefa8c966b7
Sushant Chavan <gitecsvc@gmail.com>
2018-04-04 20:00:19 +0200

    Cleanup - change tabs to whitespaces and remove unwanted new lines

--- foundations_course/service_demo/src/pose_client.py
+++ foundations_course/service_demo/src/pose_client.py
@@ -11,47 +11,47 @@ from service_demo.srv import turtle_control
 
 class PoseClient:
 
-	def __init__(self):
-		# Initialize the node
-		rospy.init_node(NODE)
+    def __init__(self):
+        # Initialize the node
+        rospy.init_node(NODE)
 
-		# Create target pose msgs
-		self.requestPositions = self.getPositions()
+        # Create target pose msgs
+        self.requestPositions = self.getPositions()
 
         #Show in the console the message
-		rospy.loginfo("Client initiated")
-		
-		#Pause the node untill the service specified(service_label) is active
-		rospy.wait_for_service("positionTurtle")
+        rospy.loginfo("Client initiated")
+        
+        #Pause the node untill the service specified(service_label) is active
+        rospy.wait_for_service("positionTurtle")
 
-		# Setup proxy for service
-		self.turtle_client = rospy.ServiceProxy("positionTurtle", turtle_control)
+        # Setup proxy for service
+        self.turtle_client = rospy.ServiceProxy("positionTurtle", turtle_control)
 
-	def getPositions(self):
-		newPositions = []
-		xvals = [2.0, 3.5, 7.0, 5.0, 1.0]
-		yvals = [7.0, 3.5, 9.0, 2.0, 0.0]
-		for i in range(0,5):
-			newPos = Pose()
-			newPos.x = xvals[i]
-			newPos.y = yvals[i]
-			newPositions.append(newPos)
-		return newPositions
+    def getPositions(self):
+        newPositions = []
+        xvals = [2.0, 3.5, 7.0, 5.0, 1.0]
+        yvals = [7.0, 3.5, 9.0, 2.0, 0.0]
+        for i in range(0,5):
+            newPos = Pose()
+            newPos.x = xvals[i]
+            newPos.y = yvals[i]
+            newPositions.append(newPos)
+        return newPositions
 
-	def requestPositioning(self):
-		for i in range(0,5): 
-			position = self.requestPositions[i]
-			print "\nClient Requesting position: ", position
-			success = self.turtle_client(position.x, position.y)
-			if success:
-				print "Position set successfully"
-			else:
-				print "Setting Position failed"
+    def requestPositioning(self):
+        for i in range(0,5): 
+            position = self.requestPositions[i]
+            print "\nClient Requesting position: ", position
+            success = self.turtle_client(position.x, position.y)
+            if success:
+                print "Position set successfully"
+            else:
+                print "Setting Position failed"
 
 if __name__ == '__main__':
-	n = PoseClient()
-	# Read in input
-	while not rospy.is_shutdown():
-		n.requestPositioning()
-		rospy.spin()
+    n = PoseClient()
+    # Read in input
+    while not rospy.is_shutdown():
+        n.requestPositioning()
+        rospy.spin()
 
--- foundations_course/service_demo/src/pose_server.py
+++ foundations_course/service_demo/src/pose_server.py
@@ -14,78 +14,78 @@ from service_demo.srv import turtle_control
 LINEAR_VELOCITY = 0.2
 
 class PoseController:
-	def __init__(self):
-		self.eps_linear = 0.1
-		self.eps_angular = 0.01
-		self.turtleReachedDestination = False
-		# Initialize the node
-		rospy.init_node(NODE)
+    def __init__(self):
+        self.eps_linear = 0.1
+        self.eps_angular = 0.01
+        self.turtleReachedDestination = False
+        # Initialize the node
+        rospy.init_node(NODE)
 
-		#Display in the console the message
-		rospy.loginfo("Server initiated")
+        #Display in the console the message
+        rospy.loginfo("Server initiated")
 
-		# Create current and target pose
-		self.current_pose = Pose()
-		self.target_pose = Pose()
+        # Create current and target pose
+        self.current_pose = Pose()
+        self.target_pose = Pose()
 
-		# Create velocity
-		self.target_vel = Twist()
+        # Create velocity
+        self.target_vel = Twist()
 
-		# Create subscriber for pose from turtle
-		self.current_pose_sub = rospy.Subscriber("turtle1/pose", Pose, self.current_pose_callback)
-		
-		self.turtle_server = rospy.Service("positionTurtle", turtle_control, self.setPosition)
+        # Create subscriber for pose from turtle
+        self.current_pose_sub = rospy.Subscriber("turtle1/pose", Pose, self.current_pose_callback)
+        
+        self.turtle_server = rospy.Service("positionTurtle", turtle_control, self.setPosition)
 
-		# Create publisher for velocity
-		self.target_vel_pub = rospy.Publisher("turtle1/cmd_vel", Twist, queue_size = 10)
+        # Create publisher for velocity
+        self.target_vel_pub = rospy.Publisher("turtle1/cmd_vel", Twist, queue_size = 10)
 
-	def current_pose_callback(self, msg):
-		self.current_pose = msg
-		self.calculate_velocity()
-		pass
+    def current_pose_callback(self, msg):
+        self.current_pose = msg
+        self.calculate_velocity()
+        pass
 
-	def setPosition(self, msg):
-		print "\nServer attempting to set position: ", msg
-		self.target_pose = Pose()
-		self.target_pose.x = msg.x
-		self.target_pose.y = msg.y
-		self.calculate_velocity()
-		while not self.turtleReachedDestination:
-			pass
-		self.turtleReachedDestination = False
-		return True
+    def setPosition(self, msg):
+        print "\nServer attempting to set position: ", msg
+        self.target_pose = Pose()
+        self.target_pose.x = msg.x
+        self.target_pose.y = msg.y
+        self.calculate_velocity()
+        while not self.turtleReachedDestination:
+            pass
+        self.turtleReachedDestination = False
+        return True
 
-	def calculate_velocity(self):
-		# Get dx, dy, dtheta
-		dx = np.abs(self.current_pose.x - self.target_pose.x)
-		dy = np.abs(self.current_pose.y - self.target_pose.y)
-		dtheta = np.abs(self.target_pose.theta - self.current_pose.theta)
-		# Check if (x,y) is okay
-		if ((dx < self.eps_linear) and (dy < self.eps_linear)):
-			self.target_vel.linear.x = 0.0
-			# We reached the correct (x,y), rotate to theta
-			if (dtheta > self.eps_angular):
-				self.target_vel.angular.z = (self.target_pose.theta - self.current_pose.theta) / 3.0
-			else:
-				self.target_vel.angular.z = 0.0
-				self.turtleReachedDestination = True
-		else:
-			# Orient towards target pose
-			#angle = np.arctan2(dx, dy) - self.current_pose.theta# + np.pi
-			angle = np.arctan2(self.target_pose.y - self.current_pose.y, self.target_pose.x - self.current_pose.x)
-			#print angle - self.current_pose.theta
-			if (np.abs(angle - self.current_pose.theta) > self.eps_angular):
-				self.target_vel.linear.x = 0.0
-				self.target_vel.angular.z = angle - self.current_pose.theta
-			else:
-				self.target_vel.linear.x = LINEAR_VELOCITY
-				self.target_vel.angular.z = 0.0
+    def calculate_velocity(self):
+        # Get dx, dy, dtheta
+        dx = np.abs(self.current_pose.x - self.target_pose.x)
+        dy = np.abs(self.current_pose.y - self.target_pose.y)
+        dtheta = np.abs(self.target_pose.theta - self.current_pose.theta)
+        # Check if (x,y) is okay
+        if ((dx < self.eps_linear) and (dy < self.eps_linear)):
+            self.target_vel.linear.x = 0.0
+            # We reached the correct (x,y), rotate to theta
+            if (dtheta > self.eps_angular):
+                self.target_vel.angular.z = (self.target_pose.theta - self.current_pose.theta) / 3.0
+            else:
+                self.target_vel.angular.z = 0.0
+                self.turtleReachedDestination = True
+        else:
+            # Orient towards target pose
+            #angle = np.arctan2(dx, dy) - self.current_pose.theta# + np.pi
+            angle = np.arctan2(self.target_pose.y - self.current_pose.y, self.target_pose.x - self.current_pose.x)
+            #print angle - self.current_pose.theta
+            if (np.abs(angle - self.current_pose.theta) > self.eps_angular):
+                self.target_vel.linear.x = 0.0
+                self.target_vel.angular.z = angle - self.current_pose.theta
+            else:
+                self.target_vel.linear.x = LINEAR_VELOCITY
+                self.target_vel.angular.z = 0.0
 
 if __name__ == '__main__':
-	n = PoseController()
-	rate = rospy.Rate(1)
-	# Read in input
-	while not rospy.is_shutdown():
-		# Publish message
-		n.target_vel_pub.publish(n.target_vel)
-		rate.sleep()
+    n = PoseController()
+    rate = rospy.Rate(1)
+    # Read in input
+    while not rospy.is_shutdown():
+        # Publish message
+        n.target_vel_pub.publish(n.target_vel)
+        rate.sleep()
